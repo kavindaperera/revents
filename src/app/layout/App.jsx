9f717e4de2eacf69de3fe3ff0baf9761c75eb4aa
kavindaperera <perera.kavinda52@gmail.com>
2020-03-19 00:49:06 +0530

    end of section 3'

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,19 @@
-import React, { Components } from 'react';
+import React, { Component } from 'react';
+import { Container, Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
 
-
-function App() {
-  return (
-    <div>
-      <h1>Re-vents</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className='app'>
+        <Container className="main">
+          <NavBar/>
+          <EventDashboard/>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
