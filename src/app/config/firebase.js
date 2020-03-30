74c3252c3ffa4e963bcea95fb820b13cedf8aaed
kavindaperera <perera.kavinda52@gmail.com>
2020-03-30 21:28:12 +0530

    end of section 16

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -9,9 +9,7 @@ const firebaseConfig = {
     databaseURL: "https://revents-271816.firebaseio.com",
     projectId: "revents-271816",
     storageBucket: "revents-271816.appspot.com",
-    messagingSenderId: "927600530437",
-    appId: "1:927600530437:web:b91621686b158164d8f86c",
-    measurementId: "G-R3VNLQ4FFP"
+    messagingSenderId: "927600530437"
 }
 
 firebase.initializeApp(firebaseConfig);
--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,32 +1,36 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
-import rootReducer from "../reducers/rootReducer";
 import thunk from 'redux-thunk';
-import firebase from '../config/firebase'
+import rootReducer from '../reducers/rootReducer';
+import firebase from '../config/firebase';
 
 const rrfConfig = {
-  userProfile: 'user',
+  userProfile: 'users',
   attachAuthIsReady: true,
-  userFirestoreForProfile: true,
+  useFirestoreForProfile: true,
   updateProfileOnLogin: false
-}
+};
 
 export const configureStore = preloadedState => {
-  const middlewares = [thunk.withExtraArgument({getFirebase, getFirestore})];
+  const middlewares = [thunk.withExtraArgument({ getFirebase, getFirestore })];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const storeEnhancers = [middlewareEnhancer];
 
-  const composedEnhancer = composeWithDevTools(...storeEnhancers, reactReduxFirebase(firebase, rrfConfig), reduxFirestore(firebase));
+  const composedEnhancer = composeWithDevTools(
+    ...storeEnhancers,
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase)
+  );
 
   const store = createStore(rootReducer, preloadedState, composedEnhancer);
 
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== 'production') {
     if (module.hot) {
-      module.hot.accept("../reducers/rootReducer.js", () => {
-        const newRootReducer = require("../reducers/rootReducer").default;
+      module.hot.accept('../reducers/rootReducer', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
         store.replaceReducer(newRootReducer);
       });
     }
